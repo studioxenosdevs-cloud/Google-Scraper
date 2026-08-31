e6ddb66beff42b3a22d2e82215af07aab4fe9e5d
studioxenosdevs-cloud <studioxenos.devs@gmail.com>
2026-08-31 21:19:58 +0500

    Update server.js

--- server.js
+++ server.js
@@ -210,5 +210,5 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 7860;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
